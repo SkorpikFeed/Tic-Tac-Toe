6566b072d8f5684ad8225b48f6fd2887799c9c6a
Taras Marchenko <tarasarar@gmail.com>
2025-11-03 20:13:21 +0200

    Add clicking functionality for the gameBoard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,26 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex, symbol) {
+    setGameBoard((gameBoard) => {
+      const updatedBoard = [...gameBoard.map((innerArray) => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((symbol, colIndex) => (
               <li key={colIndex}>
-                <button>{symbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {symbol}
+                </button>
               </li>
             ))}
           </ol>
